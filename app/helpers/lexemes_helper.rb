f4f13c94218fd9729fe9decd66870377bfbba852
Muke Tever <muke@frath.net>
2009-07-30 14:25:14 -0600

    [#29 (54)] Improved automatic unbolding around pipes in wiki markup for citation forms

--- app/helpers/lexemes_helper.rb
+++ app/helpers/lexemes_helper.rb
@@ -1,2 +1,11 @@
 module LexemesHelper
+  def unbold_pipes (str)
+    # Can't seem to do this in one regexp, so we do split/join twice.
+      str.
+        split('|').
+        join("'''|'''").
+        split(/(.*\[\[.+?)'''\|'''(.+?\]\].*)/). 
+        delete_if {|x| x == "" }.
+        join("|")
+  end
 end
