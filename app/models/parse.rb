9b57d0e8d1d91b1ae1127f17b730e47dd649934e
Muke Tever <muke@frath.net>
2009-09-17 13:48:57 -0600

    For [#33 (56)] a scope to return uninterpreted Parses

--- app/models/parse.rb
+++ app/models/parse.rb
@@ -5,6 +5,7 @@ class Parse < ActiveRecord::Base
   validates_presence_of :parsed_form
   
   named_scope :without_entries, :conditions => ['parsed_form NOT IN (SELECT form FROM "headwords" WHERE parses.parsed_form = headwords.form)']
+  named_scope :uninterpreted, :include => :interpretations, :conditions => { "interpretations.parse_id" => nil }
   
   accepts_nested_attributes_for :interpretations, :allow_destroy => true, :reject_if => proc { |attributes| attributes.all? {|k,v| v.blank?} }
     
