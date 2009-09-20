0e035c6e91b7c608f4d12fe84346d07693206762
Muke Tever <muke@frath.net>
2009-09-20 15:22:35 -0600

    Improved bold_around_pipes method for wiki display

--- app/helpers/lexemes_helper.rb
+++ app/helpers/lexemes_helper.rb
@@ -1,11 +1,16 @@
 module LexemesHelper
-  def unbold_pipes (str)
-    # Can't seem to do this in one regexp, so we do split/join twice.
-      str.
-        split('|').
-        join("'''|'''").
-        split(/(.*\[\[.+?)'''\|'''(.+?\]\].*)/). 
-        delete_if {|x| x == "" }.
-        join("|")
+  # Do wikibold around a string, except around the | character, unless it's the |
+  # that does pipe-linking.  This approach thanks to Ben Marini's suggestion at
+  # http://refactormycode.com/codes/977-ugly-split-join
+  def bold_around_pipes (str)
+    stack, functional = 0, false
+    "'''#{str}'''".each_char.inject('') do |memo, char|
+      case char
+      when '[' then stack, functional = stack + 1, true
+      when ']' then stack, functional = stack - 1, !stack.zero?
+      when '|' then stack.zero? || !functional ? char = "'''|'''" : functional = false
+      end
+      memo + char
+    end
   end
 end
