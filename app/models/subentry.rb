4386ba3f85540d3c154e96759cb841ca6a1a0c54
Muke Tever <muke@frath.net>
2009-11-02 19:11:47 -0700

    Lexeme edit form wasn't ignoring blank etymologies
    Presumably the all-blank test doesn't work since etymologies has its own nested attributes?

--- app/models/subentry.rb
+++ app/models/subentry.rb
@@ -6,5 +6,6 @@ class Subentry < ActiveRecord::Base
   has_many :senses
   validates_presence_of :paradigm
   
-  accepts_nested_attributes_for :etymologies, :senses, :allow_destroy => true, :reject_if => proc { |attributes| attributes.all? {|k,v| v.blank?} }
+  accepts_nested_attributes_for :senses, :allow_destroy => true, :reject_if => proc { |attributes| attributes.all? {|k,v| v.blank?} }
+  accepts_nested_attributes_for :etymologies, :allow_destroy => true, :reject_if => proc { |attributes| [attributes[:etymon], attributes[:source_language], attributes[:gloss]].all?(&:blank?)] }
 end
