1c40dcdd02961c3c5db6226073a5725a66ca235f
Muke Tever <muke@frath.net>
2010-07-22 19:19:13 -0600

    I knew what 'GROUP BY' was back then, didn't I?  Didn't I?

--- app/models/locus.rb
+++ app/models/locus.rb
@@ -13,6 +13,6 @@ class Locus < ActiveRecord::Base
 
   # Returns the parse with the most attestations that doesn't have an entry yet. 
   def most_wanted_parse
-    parses.find(:first, :select => '"parses".id, "parses"."parsed_form", (SELECT COUNT("parsed_form") FROM "parses" s WHERE s."parsed_form" = "parses"."parsed_form") AS count_all', :joins => ['LEFT OUTER JOIN "headwords" ON "headwords"."form" = "parsed_form"'], :conditions => {:headwords => {:form => nil}}, :order => 'count_all DESC')
+    parses.find(:first, :select => '"parses".id, "parses"."parsed_form",  COUNT("parsed_form") AS count_all', :joins => ['LEFT OUTER JOIN "headwords" ON "headwords"."form" = "parsed_form"'], :group => '"parses"."parsed_form"', :conditions => {:headwords => {:form => nil}}, :order => 'count_all DESC')
   end
 end
