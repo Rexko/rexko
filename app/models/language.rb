7e38cb212d2807e26755502863f563601a7b7bb1
Muke Tever <muke@frath.net>
2011-03-30 12:14:09 -0600

    Friendly names in dictionary index

--- app/models/language.rb
+++ app/models/language.rb
@@ -6,10 +6,18 @@ class Language < ActiveRecord::Base
   has_many :senses
   has_many :subentries
   
+  # Return the name of a language plus its ISO code if present.
   def to_s
     "%s%s" % [
       default_name,
       (" (#{iso_639_code})" if iso_639_code.present?)
     ]
+  end
+  
+  # Return the best short name of the language
+  def name
+    (default_name if default_name.present?) || 
+    (iso_639_code if iso_639_code.present?) || 
+    "Unknown language #{id}"
   end
 end
--- test/unit/language_test.rb
+++ test/unit/language_test.rb
@@ -7,4 +7,10 @@ class LanguageTest < ActiveSupport::TestCase
     assert_equal "Testian", "#{languages(:without_code)}"
     assert_equal "Testian", "#{languages(:with_blank_code)}"
   end
+  
+  test "name" do
+    assert_equal "Testwegian", languages(:testwegian).name
+    assert_equal "zxx", languages(:without_default_name).name
+    assert_equal "Unknown language 21", languages(:without_default_name_or_code).name
+  end
 end
