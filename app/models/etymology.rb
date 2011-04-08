cfa341964d3cc7d858f64e9e3d1d66d17cc924ae
Muke Tever <muke@frath.net>
2011-04-08 09:53:49 -0600

    add chained etyma to form (ugly but should work)

--- app/models/etymology.rb
+++ app/models/etymology.rb
@@ -8,6 +8,7 @@ class Etymology < ActiveRecord::Base
   
   accepts_nested_attributes_for :notes, :allow_destroy => true, :reject_if => proc { |attributes| attributes.all? {|k,v| v.blank?} }
   accepts_nested_attributes_for :parses, :allow_destroy => true, :reject_if => proc { |attributes| attributes.all? {|k,v| v.blank?} }
+  accepts_nested_attributes_for :next_etymon, :allow_destroy => true, :reject_if => proc { |attributes| attributes.all? {|k,v| v.blank?} }
   
   def self.rejectable?(attrs)
     !new(attrs.delete_if{|key, value| key == "_delete"}).valid?
