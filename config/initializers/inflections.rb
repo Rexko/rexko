da48a474f74299cdd09aeb08b0ee421e15c236e6
Muke Tever <muke@frath.net>
2011-05-07 12:01:44 -0600

    restoring inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,12 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural /^(loc)us/i, '\1i'
+  inflect.singular /^(loc)i/i, '\1us'
+  inflect.plural /^(etym)on/i, '\1a'
+  inflect.singular /^(etym)a/i, '\1on'
+  inflect.plural /^(etymothes)is/i, '\1es'
+  inflect.singular /^(etymothes)es/i, '\1is'
+end
