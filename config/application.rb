451ea2ca37298a8794e666ef7e0be869b3e2fe88
Muke Tever <muke@frath.net>
2011-05-08 09:04:31 -0600

    updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,30 +11,51 @@ module Lexicon
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
+    # See Rails::Configuration for more options.
+  
+    # Skip frameworks you're not going to use (only works if using vendor/rails).
+    # To use Rails without a database, you must remove the Active Record framework
+    # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  
+    # Only load the plugins named here, in the order given. By default, all plugins 
+    # in vendor/plugins are loaded in alphabetical order.
+    # :all can be used as a placeholder for all plugins not explicitly named
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+  
+    # Add additional load paths for your own custom dirs
+    # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  
+    # Force all environments to use the same logger level
+    # (by default production uses :info, the others :debug)
+    # config.log_level = :debug
+  
+    # Your secret key for verifying cookie session data integrity.
+    # If you change this key, all old sessions will become invalid!
+    # Make sure the secret is at least 30 characters and all random, 
+    # no regular words or you'll be exposed to dictionary attacks.
+    config.action_controller.session = {
+      :key => '_lexicon_session',
+      :secret      => '4763b5d1c3ae629e268f1e6098ae8ce4be0606c93c7e8fcdd94d9e0b6021bf343c0dc0ae0d7f00a8e11316bc1599ef1f2333f9d5050291f174c0e432736faac0'
+    }
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+  
+    # Use the database for sessions instead of the cookie-based default,
+    # which shouldn't be used to store highly confidential information
+    # (create the session table with 'rake db:sessions:create')
+    # config.action_controller.session_store = :active_record_store
+  
+    # Use SQL instead of Active Record's schema dumper when creating the test database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
+  
+    # Activate observers that should always be running
+    # config.active_record.observers = :cacher, :garbage_collector
+  
+    # Make Active Record use UTC-base instead of local time
+    # config.active_record.default_timezone = :utc
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
