360656eedeb261e81a2029f025410ce693a467e3
Muke Tever <muke@frath.net>
2011-05-08 13:48:08 -0600

    deprecations - update :joins and :conditions in gloss model

--- app/models/gloss.rb
+++ app/models/gloss.rb
@@ -4,9 +4,7 @@ class Gloss < ActiveRecord::Base
   belongs_to :language
   
   named_scope :attesting, lambda {|parsables, type|
-    { :joins => HASH_MAP_TO_PARSE, 
-      :conditions => { :parses => { :parsable_id => parsables, :parsable_type => type }}
-    }
+    joins(HASH_MAP_TO_PARSE).where({ :parses => { :parsable_id => parsables, :parsable_type => type }})
   }
   
   HASH_MAP_TO_PARSE = { :sense => Sense::HASH_MAP_TO_PARSE }
