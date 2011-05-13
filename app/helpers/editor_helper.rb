8b08038a918e0475a6ed3e071b7d09a15937aa7a
Muke Tever <muke@frath.net>
2011-05-12 19:56:25 -0600

    Array#rand -> Array#sample

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -11,7 +11,7 @@ module EditorHelper
     '<ul><li>%s</li><li>%s</li></ul>' % [
       ("0 " << link_to(unattested.form, unattested.lexeme) if unattested),
       [[1, 4], [5, 9], [10, 49], [50, 100]].collect {|num|
-        Parse.popularity_between(num[0], num[1]).rand
+        Parse.popularity_between(num[0], num[1]).sample
       }.flatten.collect{|parse|
         parse.count_all << " " << headword_link(parse) if parse
       }.join("</li><li>") 
