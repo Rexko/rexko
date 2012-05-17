ecea79a8274c87d2f380861cd5e1d422553592c8
Muke Tever <muke@frath.net>
2012-05-17 12:00:46 -0600

    Basic listing in parsable (to replace unattached)

--- app/controllers/parsable_controller.rb
+++ app/controllers/parsable_controller.rb
@@ -1,5 +1,10 @@
 class ParsableController < ApplicationController
+  layout '1col_layout'
+
   def index
+  	forms = Parse.forms_of(Parse.uninterpreted.first)
+  	@parsables = Parse.unattached_to(forms).paginate(:page => params[:page], :per_page => 5)
+  	@page_title = "Unattached to %s" % forms.to_sentence
   end
 
 end
--- app/helpers/parsable_helper.rb
+++ app/helpers/parsable_helper.rb
@@ -1,2 +1,10 @@
 module ParsableHelper
+	def display_obj_of parse
+		case parse.parsable_type
+		when "Attestation"
+			parse.parsable.locus || parse.parsable
+		else
+			parse.parsable
+		end	
+	end
 end
