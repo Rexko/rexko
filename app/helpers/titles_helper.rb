521f47d25b86529d923e6058a78acf01d3f8c3bd
Luis Rivera <theluisrivera@gmail.com>
2014-04-15 18:19:01 -0600

    "if" != "if present?"

--- app/helpers/titles_helper.rb
+++ app/helpers/titles_helper.rb
@@ -1,10 +1,10 @@
 module TitlesHelper
   def htmlize_title title
     sanitize \
-    "<i>#{"<a href=\"" + h(title.url) + "\">" if title.url}#{title.name}#{"</a>" if title.url}</i>" \
-    "#{" (#{title.publication_year})" if title.publication_year}"                                   \
-    "#{", #{title.publication_place}#{": " if title.publisher}" if title.publication_place }"       \
-    "#{"#{", " unless title.publication_place}#{title.publisher}" if title.publisher }"             \
+    "<i>#{"<a href=\"" + h(title.url) + "\">" if title.url.present?}#{title.name}#{"</a>" if title.url.present?}</i>" \
+    "#{" (#{title.publication_year})" if title.publication_year.present?}"                                            \
+    "#{", #{title.publication_place}#{": " if title.publisher.present?}" if title.publication_place.present? }"       \
+    "#{"#{", " unless title.publication_place.present?}#{title.publisher}" if title.publisher.present? }"             \
     "."
   end
 end
