69dc169aeb771dd01bf7d47d5172b43df6cdcbf6
Luis Rivera <theluisrivera@gmail.com>
2014-04-23 17:58:59 -0600

    Add etymology sources to lexeme edit form

--- app/models/etymothesis.rb
+++ app/models/etymothesis.rb
@@ -2,4 +2,7 @@ class Etymothesis < ActiveRecord::Base
   belongs_to :etymology
   belongs_to :subentry
   belongs_to :source
+  
+  accepts_nested_attributes_for :etymology, allow_destroy: true, reject_if: proc {|attrs| Etymology.rejectable?(attrs) }
+  accepts_nested_attributes_for :source, allow_destroy: false, reject_if: :all_blank
 end
