11738bb4fe96aac400a14fedbef2ea6210ac3a48
Luis Rivera <theluisrivera@gmail.com>
2014-08-09 10:23:23 -0600

    locale and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Lexicon::Application.routes.draw do
-  get "parsable/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,46 +46,51 @@ Lexicon::Application.routes.draw do
   #     resources :products
   #   end
 
-  match 'lexemes/matching(/:matchtype)/:headword', :to => 'lexemes#matching', :as => :matching, :defaults => { :matchtype => Lexeme::SUBSTRING }
-  match 'author(/:author(/:page))' => 'loci#matching'
-  match 'loci/show_by_author(/:author)' => 'loci#show_by_author'
-  match 'unattached/matching/:forms' => 'loci#unattached'
-  match 'unattached/:id' => 'loci#unattached'
-  match 'html(/:headword)' => 'lexemes#show_by_headword', :as => :exact_lexeme
-  resources :sort_orders
-  resources :notes
-  resources :interpretations
-  resources :parses
-  resources :attestations
-  resources :orthographs
-  resources :etymotheses
-  resources :dictionary_scopes
-  resources :authorships do
-    post 'matching', on: :collection
-  end
-  resources :authors do
-    post 'matching', on: :collection
-  end
-  resources :titles do
-    post 'matching', on: :collection
-  end
-  resources :sources
-  resources :loci
-  match 'loci(/index(/*loci))' => 'loci#index'  #replace this
-  resources :glosses
-  resources :senses
-  resources :etymologies
-  resources :phonetic_forms
-  resources :headwords
-  resources :subentries
-  resources :lexemes
-  resources :dictionaries
-  resources :languages
+	scope "/(:locale)" do
+    get "parsable/index" => 'parsable#index'
+    match 'lexemes/matching(/:matchtype)/:headword', :to => 'lexemes#matching', :as => :matching, :defaults => { :matchtype => Lexeme::SUBSTRING }
+    match 'author(/:author(/:page))' => 'loci#matching'
+    match 'loci/show_by_author(/:author)' => 'loci#show_by_author'
+    match 'unattached/matching/:forms' => 'loci#unattached'
+    match 'unattached/:id' => 'loci#unattached'
+    match 'html(/:headword)' => 'lexemes#show_by_headword', :as => :exact_lexeme
+    resources :sort_orders
+    resources :notes
+    resources :interpretations
+    resources :parses
+    resources :attestations
+    resources :orthographs
+    resources :etymotheses
+    resources :dictionary_scopes
+    resources :authorships do
+      post 'matching', on: :collection
+    end
+    resources :authors do
+      post 'matching', on: :collection
+    end
+    resources :titles do
+      post 'matching', on: :collection
+    end
+    resources :sources
+    resources :loci
+    match 'loci(/index(/*loci))' => 'loci#index'  #replace this
+    resources :glosses
+    resources :senses
+    resources :etymologies
+    resources :phonetic_forms
+    resources :headwords
+    resources :subentries
+    resources :lexemes
+    resources :dictionaries
+    resources :languages
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-  root :to => 'editor#index'
+    # You can have the root of your site routed with "root"
+    # just remember to delete public/index.html.
+    # root :to => "welcome#index"
+    root :to => 'editor#index'
+  end
+
+  match '/:locale' => 'editor#index'
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
