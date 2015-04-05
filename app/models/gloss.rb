26853a8df4fc394d34529642038c237513cb08ad
Luis Rivera <theluisrivera@gmail.com>
2015-04-04 18:35:15 -0600

    greening tests
    - apparently attrs[:gloss] works on an actual Gloss, but select doesn’t.
    - add und to fixtures as needed (probably)

--- app/models/gloss.rb
+++ app/models/gloss.rb
@@ -26,6 +26,7 @@ class Gloss < ActiveRecord::Base
   # Determine whether we should hit reject_if when something accepts_nested_attributes_for glosses.
   def self.rejectable?(attrs)
     # attrs[:gloss].blank? # before we had i18n
+    attrs = attrs.attributes if attrs.is_a? Gloss
     attrs.select {|k,v| k.start_with?("gloss_")}.all? {|k,v| v.blank?}
   end
 end
