a55ada8453648db4085b05d8372ea6686edb4a29
Luis Rivera <theluisrivera@gmail.com>
2015-05-01 15:34:17 -0600

    know locale

--- app/models/lexeme.rb
+++ app/models/lexeme.rb
@@ -26,7 +26,7 @@ class Lexeme < ActiveRecord::Base
   end
   
   def primary_headword
-    headwords.order('acceptance DESC').first.try(:form)
+    Globalize.with_locale(language.iso_639_code) { headwords.order('acceptance DESC').first.try(:form) }
   end
   
   def loci(options = {})
