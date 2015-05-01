16bc0c2c49fbb0fa27c3b76acf926ef880090804
Luis Rivera <theluisrivera@gmail.com>
2015-05-01 15:32:38 -0600

    make langs available in lex/dict show

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -33,6 +33,7 @@ class DictionariesController < ApplicationController
     unsorted_lexemes = @dictionary.lexemes.includes([{:dictionaries => [:source_language]}, {:headwords => [:phonetic_forms, :language]}, {:subentries => [{:senses => [{:glosses => :language}, :notes, :language]}, {:etymologies => [:notes, :original_language, {:next_etymon => [:notes, :original_language, :next_etymon] }]}, {:notes => :language}, :language]}])
     @lexemes = @source_language.sort(unsorted_lexemes, by: :primary_headword)
     @page_title = @dictionary.title
+    @langs = Dictionary.langs_hash_for(@dictionary)
 
     respond_to do |format|
       format.html # show.html.erb
