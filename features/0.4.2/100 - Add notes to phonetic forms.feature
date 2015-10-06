dccfcb296788f1f4b7c4518785a8666c055c6c4e
Luis R <theluisrivera@gmail.com>
2015-10-06 00:15:17 -0600

    failing test

---
+++ features/0.4.2/100 - Add notes to phonetic forms.feature
@@ -0,0 +1,9 @@
+Feature: Adding notes to phonetic forms
+    The lexeme edit form must include the capacity to add notes
+    to phonetic forms. 
+
+    Scenario:
+        Given I am on the lexeme edit form
+         When I have added a phonetic form
+         Then I should see a link to add a note
+          
---
+++ features/0.4.2/100_steps.rb
@@ -0,0 +1,25 @@
+Language::DEFAULT.save
+
+Given "I am on the lexeme edit form" do  
+  # At this point in time lexemes#new fails when there's no dictionaries present
+  Dictionary.create(title: "Test", language: Language.first, source_language: Language.first, target_language: Language.first)
+
+  visit url_for(
+    controller: :lexemes, 
+    action: :new, 
+    locale: I18n.default_locale, 
+    host: "127.0.0.1", 
+    port: 7787
+  )
+  
+  @note_link_count = page.all(I18n.t("helpers.link_to_add.note")).size
+end
+
+When "I have added a phonetic form" do 
+  page.click_link(I18n.t("helpers.link_to_add.phonetic form"))
+end
+
+Then "I should see a link to add a note" do
+  assert page.has_text(I18n.t("helpers.link_to_add.note"), count: @note_link_count + 1)
+end
+  
