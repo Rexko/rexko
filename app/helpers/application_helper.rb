97d11792b8f80ad5cf0bf8d068625a3a6f302e88
Luis R <theluisrivera@gmail.com>
2015-10-06 23:27:01 -0600

    comment, sort of

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -254,6 +254,7 @@ module ApplicationHelper
     end
   end
   
+  # Format of data entry fields for items appearing in multiple languages.
   def translatable_tag form, field, attribute, languages = [], html_options = {}
     content_tag(:div, {class: "translatable", data: { languages: languages }}) do
       default_locale = I18n.default_locale.to_s
