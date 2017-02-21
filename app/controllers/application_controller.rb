4b447ba80c88110d1797d09638ca034f44daa428
Luis <theluisrivera@gmail.com>
2017-02-20 18:07:44 -0800

    lint application_controller.rb
    trailing space, unused argument, commt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
+# Settings to be inherited by other controllers
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   before_filter :set_locale
-  
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-  
-  def default_url_options(options = {})
+
+  def default_url_options(*)
     { locale: I18n.locale }
   end
 end
