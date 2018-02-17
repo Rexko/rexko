ef02f63f5c0359789288cd1c4b01f9808deb27aa
Luis <theluisrivera@gmail.com>
2018-02-16 21:38:29 -0800

    allow arbitrary locales

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module Lexicon
     # config.i18n.default_locale = :de
     
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.fallbacks = true
+    config.i18n.enforce_available_locales = false
   end
 end
