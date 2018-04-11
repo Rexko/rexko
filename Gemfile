a4d3687b60dec90897c095950ba975dc7c331172
Luis <theluisrivera@gmail.com>
2018-04-10 21:21:04 -0700

    update to rails 4.2

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 # This preamble is the current preamble for Rails 3 apps; edit as needed.
 
 source 'http://rubygems.org'
-gem 'rails', '4.1.16'
+gem 'rails', '4.2.10'
 
 gem 'globalize', '~> 4.0.3'
 gem 'globalize-accessors'
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Lexicon
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.fallbacks = true
     config.i18n.enforce_available_locales = false
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
