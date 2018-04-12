3609872a2ac06bf0544d7df1360d19d51bbcbc48
Luis <theluisrivera@gmail.com>
2018-04-11 19:40:51 -0700

    deprecation

--- test/functional/editor_controller_test.rb
+++ test/functional/editor_controller_test.rb
@@ -8,9 +8,9 @@ class EditorControllerTest < ActionController::TestCase
   
   test "should contain section links" do
     get :index
-    assert_tag :a, :attributes => { :href => languages_path }
-    assert_tag :a, :attributes => { :href => dictionaries_path }
-    assert_tag :a, :attributes => { :href => lexemes_path }
-    assert_tag :a, :attributes => { :href => loci_path }
+    assert_select "a[href=?]", languages_path
+    assert_select "a[href=?]", dictionaries_path
+    assert_select "a[href=?]", lexemes_path
+    assert_select "a[href=?]", loci_path
   end
 end
