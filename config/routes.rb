97e149d17586a150506269087f2950e9ff28cf68
Luis <theluisrivera@gmail.com>
2018-05-04 19:44:28 -0700

    ran app:update

--- config/routes.rb
+++ config/routes.rb
@@ -90,4 +90,5 @@ Rails.application.routes.draw do
     
     root to: 'editor#index'
   end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
